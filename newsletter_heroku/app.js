1162087d49001a7a411b60438641b54f129f8d5a
hao134 <hao134@github.com>
2022-08-27 18:39:02 +0800

    modify some errors

--- newsletter_heroku/app.js
+++ newsletter_heroku/app.js
@@ -41,7 +41,7 @@ app.post('/', function(req, res) {
  
   const options = {
     method: "POST",
-    auth: "shihhao:2e4d067aba7b0c75a213067512af6fa4-us10"
+    auth: "Auth"
   }
  
   const request = https.request(url, options, function(response) {
@@ -68,62 +68,4 @@ app.listen(process.env.PORT || 5000, function(){
     console.log("server is running on port 5000.");
 });
 
-// ---------------------------------------------------------------
-// another version
 
-// const express = require("express");
-// const client = require("@mailchimp/mailchimp_marketing");
- 
-// const app = express();
- 
-// app.use(express.static("public"));        // this allows to import css from computer
-// app.use(express.urlencoded({extended: true}));
- 
-// app.get("/", function (req, res) {
-//     res.sendFile(__dirname + "/signup.html");
-// });
- 
-// client.setConfig({
-//   apiKey: "8d4818c25728cea8aa17d6d31f740c1f-us10",
-//   server: "us10", // example : us12
-// });
- 
-// app.post("/", function (req, res) {
-//     const firstName = req.body.fname;
-//     const lastName = req.body.lname;
-//     const email = req.body.email;
- 
-//     async function run() {
-//         const response = await client.lists.addListMember("1fb64a86b6", {
-//             email_address: email,
-//             status: "subscribed",
-//             merge_fields: {
-//                 FNAME: firstName,
-//                 LNAME: lastName
-//             }
-//             // members: [{
-//             //     email_address: email,
-//             //     status: "subscribed",
-//             //     merge_fields: {
-//             //         FNAME: firstName,
-//             //         LNAME: lastName
-//             //     }
-//             // }]
-//           });
-//         console.log(`Successfully added contact as an audience member. The contact's id is ${response.id}.`);
-//         res.sendFile(__dirname + "/success.html");
-//     }
-//     run().catch(e => res.sendFile(__dirname + "/failure.html"));
-// });
-
-
-
-// app.listen(5000, function(){
-//     console.log("server is running on port 5000.");
-// });
-
-// API KEY
-// 8d4818c25728cea8aa17d6d31f740c1f-us10
-
-// LIST ID
-// 1fb64a86b6
